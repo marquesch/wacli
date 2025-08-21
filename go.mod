4e695434b00d6be2de2db8bf29946ca61a40378b
Carlos Marques <chmarques93@gmail.com>
2025-08-21 00:01:38 -0300

    basic implementation of send message

--- go.mod
+++ go.mod
@@ -8,14 +8,20 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/mdp/qrterminal/v3 v3.2.1 // indirect
+	github.com/petermattis/goid v0.0.0-20250813065127-a731cc31b4fe // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/urfave/cli/v3 v3.4.1 // indirect
 	go.mau.fi/libsignal v0.2.0 // indirect
 	go.mau.fi/util v0.9.0 // indirect
-	go.mau.fi/whatsmeow v0.0.0-20250816112049-1b82e4b52df1 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20250820160106-21f5124c7602 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/exp v0.0.0-20250813145105-42675adae3e6 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/term v0.34.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	google.golang.org/protobuf v1.36.7 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,120 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
 	"os"
-	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"github.com/urfave/cli/v3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
 )
 
+func connect() (*whatsmeow.Client, error) {
+	dbLog := waLog.Stdout("Database", "ERROR", true)
+
+	ctx := context.Background()
+	container, err := sqlstore.New(ctx, "sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		return nil, fmt.Errorf("error setting db: %w", err)
+	}
+
+	deviceStore, err := container.GetFirstDevice(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error setting device: %w", err)
+	}
+
+	clientLog := waLog.Stdout("Client", "ERROR", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+
+		err = client.Connect()
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to client: %w", err)
+		}
+
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			}
+		}
+
+	} else {
+		err = client.Connect()
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to client: %w", err)
+		}
+	}
+
+	return client, nil
+}
+
+func sendTextMessage(client *whatsmeow.Client, recipient string, text string) error {
+	toJID, _ := types.ParseJID(fmt.Sprintf("%s@s.whatsapp.net", recipient))
+
+	message := &waE2E.Message{
+		Conversation: proto.String(text),
+	}
+
+	_, err := client.SendMessage(context.Background(), toJID, message)
+	if err != nil {
+		return fmt.Errorf("error sending message: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
-	file, err := os.OpenFile("/var/log/wasvc/wasvc.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	client, err := connect()
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
 
-	log.SetOutput(file)
-	for {
-		log.Println("Service is still up and running")
-		time.Sleep(time.Second)
+	var recipient string
+	var text string
+
+	cmd := &cli.Command{
+		Name:  "wacli",
+		Usage: "whatsapp cli tool",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "send",
+				Commands: []*cli.Command{
+					{
+						Name: "text",
+						Arguments: []cli.Argument{
+							&cli.StringArg{
+								Name:        "recipient",
+								Destination: &recipient,
+							},
+							&cli.StringArg{
+								Name:        "text",
+								Destination: &text,
+							},
+						},
+						Action: func(ctx context.Context, cmd *cli.Command) error {
+							fmt.Printf("inside action\ntext: %s\nrecipient: %s", text, recipient)
+							err := sendTextMessage(client, recipient, text)
+							return err
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		panic(err)
 	}
 }
