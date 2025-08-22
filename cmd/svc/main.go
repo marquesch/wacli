83ce75a7bf12cf7cc3a5ca5d189dbe77bbcb45b5
Carlos Marques <chmarques93@gmail.com>
2025-08-22 13:57:48 -0300

    message sending done

--- cmd/svc/main.go
+++ cmd/svc/main.go
@@ -2,17 +2,92 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net"
 	"os"
 	"time"
 
+	"context"
+
 	"github.com/marquesch/wasvc/internal/socket"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
 )
 
 const socketPath = "/tmp/app.sock"
 
+func whatsappConnect() (*whatsmeow.Client, error) {
+	dbLog := waLog.Stdout("Database", "ERROR", true)
+
+	ctx := context.Background()
+	container, err := sqlstore.New(ctx, "sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		return nil, fmt.Errorf("error setting db: %w", err)
+	}
+
+	deviceStore, err := container.GetFirstDevice(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error setting device: %w", err)
+	}
+
+	clientLog := waLog.Stdout("Client", "ERROR", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+
+		err = client.Connect()
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to client: %w", err)
+		}
+
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+			}
+		}
+
+	} else {
+		err = client.Connect()
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to client: %w", err)
+		}
+	}
+
+	return client, nil
+}
+
+func sendTextMessage(client *whatsmeow.Client, recipient string, text string) error {
+	toJID, _ := types.ParseJID(fmt.Sprintf("%s@s.whatsapp.net", recipient))
+
+	message := &waE2E.Message{
+		Conversation: proto.String(text),
+	}
+
+	_, err := client.SendMessage(context.Background(), toJID, message)
+	if err != nil {
+		return fmt.Errorf("error sending message: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
+	waClient, err := whatsappConnect()
+	if err != nil {
+		fmt.Println("Error connecting to whatsapp: ", err)
+		os.Exit(1)
+	}
+	fmt.Println("connected successfully")
+	defer waClient.Disconnect()
+
 	if err := os.RemoveAll(socketPath); err != nil {
 		fmt.Println("Error removing old socket:", err)
 		os.Exit(1)
@@ -23,11 +98,8 @@ func main() {
 		fmt.Println("Listen error:", err)
 		os.Exit(1)
 	}
-
-	defer func() {
-		listener.Close()
-		os.Remove(socketPath)
-	}()
+	defer listener.Close()
+	defer os.Remove(socketPath)
 
 	for {
 		conn, err := listener.Accept()
@@ -35,31 +107,37 @@ func main() {
 			fmt.Println("Accept error:", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, waClient)
 	}
 }
 
-func handleConnection(conn net.Conn) error {
+func handleConnection(conn net.Conn, waClient *whatsmeow.Client) error {
 	defer conn.Close()
 
 	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 
-	message, err := socket.ReadMessage(conn)
+	var clientEvent socket.ClientEvent
+	err := socket.ReadEvent(conn, &clientEvent)
 	if err != nil {
 		return fmt.Errorf("error reading message: %w", err)
 	}
 
-	var response socket.SocketMessage
+	var response socket.ServerEvent
 
-	if rand.Intn(100) > 50 {
-		response.Message = fmt.Sprintf("success! your message was: %s", message.Message)
-		response.ErrStr = ""
-	} else {
-		response.Message = fmt.Sprintf("failed! your message was: %s", message.Message)
-		response.ErrStr = "faileddddddd. good luck next time"
+	if clientEvent.Command == "send" {
+		recipient := clientEvent.Args[0]
+		body := clientEvent.Args[1]
+
+		response.Success = true
+
+		err = sendTextMessage(waClient, recipient, body)
+		if err != nil {
+			fmt.Println(err)
+			response.Success = false
+		}
 	}
 
-	err = socket.WriteMessage(conn, response)
+	err = socket.WriteEvent(conn, response)
 	if err != nil {
 		return fmt.Errorf("error writing message: %w", err)
 	}
--- internal/socket/socket.go
+++ internal/socket/socket.go
@@ -6,30 +6,36 @@ import (
 	"net"
 )
 
-type SocketMessage struct {
-	Message string `json:"message"`
-	ErrStr  string `json:"err_str"`
+type ClientEvent struct {
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
 }
 
-func ReadMessage(conn net.Conn) (SocketMessage, error) {
-	var socketMessage SocketMessage
+type ServerEvent struct {
+	Success bool `json:"success"`
+}
 
+type Event interface {
+	ClientEvent | ServerEvent
+}
+
+func ReadEvent[T Event](conn net.Conn, event *T) error {
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
-		return socketMessage, fmt.Errorf("error reading from connection: %w", err)
+		return fmt.Errorf("error reading from connection: %w", err)
 	}
 
-	err = json.Unmarshal(buf[:n], &socketMessage)
+	err = json.Unmarshal(buf[:n], &event)
 	if err != nil {
-		return socketMessage, fmt.Errorf("error unmarshaling json: %w", err)
+		return fmt.Errorf("error unmarshaling json: %w", err)
 	}
 
-	return socketMessage, nil
+	return nil
 }
 
-func WriteMessage(conn net.Conn, socketMessage SocketMessage) error {
-	message, err := json.Marshal(socketMessage)
+func WriteEvent[T Event](conn net.Conn, event T) error {
+	message, err := json.Marshal(event)
 	if err != nil {
 		return fmt.Errorf("error marshaling json: %w", err)
 	}
