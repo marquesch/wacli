228cee88c1d6eda2e95c1d9ed76f8e5365434abb
Carlos Marques <chmarques93@gmail.com>
2025-08-22 18:00:19 -0300

    done with basic cli send text message

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,41 +1,82 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
 
 	"github.com/marquesch/wasvc/internal/socket"
+	"github.com/urfave/cli/v3"
 )
 
 const socketPath = "/tmp/app.sock"
 
 func main() {
-	conn, err := net.Dial("unix", socketPath)
-	if err != nil {
-		fmt.Println("Dial error:", err)
-		os.Exit(1)
+	var recipient string
+	var text string
+
+	cmd := &cli.Command{
+		Name:  "wacli",
+		Usage: "whatsapp cli tool",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "send",
+				Commands: []*cli.Command{
+					{
+						Name: "text",
+						Arguments: []cli.Argument{
+							&cli.StringArg{
+								Name:        "recipient",
+								Destination: &recipient,
+							},
+							&cli.StringArg{
+								Name:        "text",
+								Destination: &text,
+							},
+						},
+						Action: func(ctx context.Context, cmd *cli.Command) error {
+
+							conn, err := net.Dial("unix", socketPath)
+							if err != nil {
+								fmt.Println("Dial error:", err)
+								os.Exit(1)
+							}
+							defer conn.Close()
+
+							message := socket.ClientEvent{
+								Command: "send",
+								Args:    []string{recipient, text},
+							}
+
+							err = socket.WriteEvent(conn, message)
+							if err != nil {
+								fmt.Println("write error: ", err)
+								os.Exit(1)
+							}
+
+							var response socket.ServerEvent
+
+							err = socket.ReadEvent(conn, &response)
+							if err != nil {
+								fmt.Println("read error: ", err)
+								os.Exit(1)
+							}
+
+							fmt.Printf("got succesful response: %t", response.Success)
+
+							return nil
+						},
+					},
+				},
+			},
+		},
 	}
-	defer conn.Close()
 
-	message := socket.ClientEvent{
-		Command: "send",
-		Args:    []string{"number", "msg"},
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		panic(err)
 	}
-
-	err = socket.WriteEvent(conn, message)
-	if err != nil {
-		fmt.Println("write error: ", err)
-		os.Exit(1)
-	}
-
-	var response socket.ServerEvent
-
-	err = socket.ReadEvent(conn, &response)
-	if err != nil {
-		fmt.Println("read error: ", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("got succesful response: %t", response.Success)
 }
